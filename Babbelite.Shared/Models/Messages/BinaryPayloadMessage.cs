dfad822577600bcd1116462ff1625cca6ea20726
Frooxius <tomas.mariancik@gmail.com>
2026-04-17 23:17:29 +0200

    feat: Add binary paypload message

---
+++ Babbelite.Shared/Models/Messages/BinaryPayloadMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Babbelite.Shared
+{
+    /// <summary>
+    /// Represents a message with a binary payload. This payload is sent as a separate WebSocket binary
+    /// message that follows immediately after this one.
+    /// </summary>
+    public abstract class BinaryPayloadMessage : Message
+    {
+        [JsonIgnore]
+        public byte[] RawBinaryPayload { get; set; }
+    }
+}
--- Babbelite.Shared/Models/Messages/Transcribing/PushLiveTranscribeAudioData.cs
+++ Babbelite.Shared/Models/Messages/Transcribing/PushLiveTranscribeAudioData.cs
@@ -1,22 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Json.Serialization;
 
 namespace Babbelite.Shared
 {
+    [JsonConverter(typeof(JsonStringEnumConverter<AudioDataEncoding>))]
+    public enum AudioDataEncoding
+    {
+        /// <summary>
+        /// Uncompressed raw PCM samples. These are simple to work with, but not very network efficient.
+        /// Should be OK on LAN
+        /// </summary>
+        RawPCM
+    }
+
     /// <summary>
     /// Used to push a chunk of audio data into the live transcribe session.
     /// It's not expected to immediately receive a response - the system can buffer it for as long as it
     /// needs for analysis. Responses are sent asynchronously. 
     /// Generally you'll want to push the audio data into the session as soon as they come in.
     /// </summary>
-    public class PushLiveTranscribeAudioData : Message
+    public class PushLiveTranscribeAudioData : BinaryPayloadMessage
     {
         /// <summary>
         /// Unique ID of the session into which to push the audio data into
         /// </summary>
         [JsonPropertyName("sessionId")]
         public string SessionId { get; set; }
+
+        /// <summary>
+        /// Encoding of the audio data
+        /// </summary>
+        [JsonPropertyName("encoding")]
+        public AudioDataEncoding Encoding { get; set; }
+
+        public void SetRawAudioData(Span<float> samples)
+        {
+            RawBinaryPayload = new byte[samples.Length * sizeof(float)];
+            Encoding = AudioDataEncoding.RawPCM;
+
+            samples.CopyTo(MemoryMarshal.Cast<byte, float>(RawBinaryPayload.AsSpan()));
+        }
+
+        public Span<float> GetRawAudioData()
+        {
+            if (Encoding != AudioDataEncoding.RawPCM)
+                throw new InvalidOperationException("The audio data is not raw PCM");
+
+            return MemoryMarshal.Cast<byte, float>(RawBinaryPayload.AsSpan());
+        }
     }
 }
